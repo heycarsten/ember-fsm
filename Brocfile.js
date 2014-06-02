a193490908179bb77524dbca4a401a4450d9b554
Sjoerd de Jong <sjoerd@smwm.nl>
2014-06-02 15:10:52 +0200

    Updated Broccoli

--- Brocfile.js
+++ Brocfile.js
@@ -1,13 +1,11 @@
-module.exports = function(broccoli) {
-  return require('broccoli-dist-es6-module')(broccoli.makeTree('lib'), {
-    global:      'Ember.FSM',
-    packageName: 'ember-fsm',
-    main:        'main',
+module.exports = require('broccoli-dist-es6-module')('lib', {
+  global:      'Ember.FSM',
+  packageName: 'ember-fsm',
+  main:        'main',
 
-    shim: {
-      'ember':        'Ember',
-      'ember/rsvp':   'Ember.RSVP',
-      'ember/string': 'Ember.String'
-    }
-  });
-};
+  shim: {
+    'ember':        'Ember',
+    'ember/rsvp':   'Ember.RSVP',
+    'ember/string': 'Ember.String'
+  }
+});
